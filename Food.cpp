26a9a1fa1fd24a23c042c012d745eeeccf728d0d
Jenisha Thevarajah <144248040+thevaj5@users.noreply.github.com>
2023-12-01 19:20:33 -0500

    Add files via upload

--- Food.cpp
+++ Food.cpp
@@ -2,63 +2,96 @@
 #include "MacUILib.h"
 #include "Food.h"
 
+
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 
+
 using namespace std;
+
 
 Food::Food(GameMechs* thisGMRef)
 {
-    //game mech reference input - pointer
-    //assign pointer to input
-    mainGameMechsRef = thisGMRef;
-    foodPos.setObjPos(mainGameMechsRef->getBoardSizeX()/2, mainGameMechsRef->getBoardSizeY()/2, '&');
+   //game mech reference input - pointer
+   //assign pointer to input
+   mainGameMechsRef = thisGMRef;
+   foodPos.setObjPos(mainGameMechsRef->getBoardSizeX()/2, mainGameMechsRef->getBoardSizeY()/2, 'o');
 }
+
 
 Food::~Food()
 {
-    cout << "Destructor Called" << endl;
+   cout << "Destructor Called" << endl;
 }
 
-void Food::generateFood(objPos blockOff)
+
+void Food::generateFood(objPosArrayList& playerPosList)
 {
 
-    srand(time(NULL));
-    int i, j;
-    int candidate = 0;
-    int candidateCoords[10][30];
+   srand(time(NULL));
+   int i, j;
+   int isEqual = 0;
+   int candidate = 0;
+   int candidateCoords[10][30];
+   int flag = 0;
 
-    for (i = 0; i < 10; i++) {
-        for (j = 0; j < 30; j++) {
-            candidateCoords[i][j] = 0;
-        }
+
+   for (i = 0; i < 10; i++) {
+       for (j = 0; j < 30; j++) {
+           candidateCoords[i][j] = 0;
+       }
+   }
+
+
+   do {
+       foodPos.x = rand() % mainGameMechsRef->getBoardSizeX();
+       foodPos.y = rand() % mainGameMechsRef->getBoardSizeY();
+
+
+       // Ensure food position is within bounds
+       foodPos.x = max(1, min(foodPos.x, mainGameMechsRef->getBoardSizeX() - 2));
+       foodPos.y = max(1, min(foodPos.y, mainGameMechsRef->getBoardSizeY() - 2));
+
+
+for (i = 0; i < playerPosList.getSize(); i++)
+{
+
+    if (i >= playerPosList.getSize()) {
+        // Additional safety check to avoid going out of bounds
+        break;
     }
-
-    do {
-        foodPos.x = rand() % mainGameMechsRef->getBoardSizeX();
-        foodPos.y = rand() % mainGameMechsRef->getBoardSizeY();
-
-        // Ensure food position is within bounds
-        foodPos.x = std::max(1, std::min(foodPos.x, mainGameMechsRef->getBoardSizeX() - 2));
-        foodPos.y = std::max(1, std::min(foodPos.y, mainGameMechsRef->getBoardSizeY() - 2));
+    objPos tempPos;
+    playerPosList.getElement(tempPos, i);
 
 
-    } while (foodPos.isPosEqual(&blockOff) ||
-             foodPos.x == 0 || foodPos.y == 0 ||
-             foodPos.x == mainGameMechsRef->getBoardSizeX() - 1 ||
-             foodPos.y == mainGameMechsRef->getBoardSizeY() - 1 ||
-             candidateCoords[foodPos.x][foodPos.y] != 0);
+   if (tempPos.isPosEqual(&foodPos))
+   {
+      flag = 1;
+      
+   }
+}
 
-    // Mark the generated coordinates as used
-    candidateCoords[foodPos.x][foodPos.y] = 1;
 
-    // Print a message to indicate that the food has been generated
-    cout << "Food generated successfully at position: (" << foodPos.x << ", " << foodPos.y << ")" << endl;
+//go through all position
+   } while (flag == 1 ||
+            foodPos.x == 0 || foodPos.y == 0 ||
+            foodPos.x == mainGameMechsRef->getBoardSizeX() - 1 ||
+            foodPos.y == mainGameMechsRef->getBoardSizeY() - 1 ||
+            candidateCoords[foodPos.x][foodPos.y] != 0);
+
+
+   // Mark the generated coordinates as used
+   candidateCoords[foodPos.x][foodPos.y] = 1;
+
+
+   // Print a message to indicate that the food has been generated
+   cout << "Food generated successfully at position: (" << foodPos.x << ", " << foodPos.y << ")" << endl;
 }
 void Food::getFoodPos(objPos &returnPos)
 {
-    // Step 2: Food objPos getter method
-    returnPos = foodPos;
+   // Step 2: Food objPos getter method
+   returnPos = foodPos;
+
 
 }
