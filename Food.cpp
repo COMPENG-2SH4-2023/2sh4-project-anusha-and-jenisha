334069ba081a65aa041b098d43b2b93fa9bdab9a
Jenisha Thevarajah <144248040+thevaj5@users.noreply.github.com>
2023-12-02 12:10:34 -0500

    Add files via upload

--- Food.cpp
+++ Food.cpp
@@ -29,69 +29,33 @@ Food::~Food()
 void Food::generateFood(objPosArrayList& playerPosList)
 {
 
-   srand(time(NULL));
-   int i, j;
-   int isEqual = 0;
-   int candidate = 0;
-   int candidateCoords[10][30];
-   int flag = 0;
+    srand(time(NULL));
+    int flag = 0;
+    int i;
+    bool overlap = true;
 
+    do {
+        // Generate random coordinates within bounds
+        foodPos.x = rand() % (mainGameMechsRef->getBoardSizeX() - 2) + 1;
+        foodPos.y = rand() % (mainGameMechsRef->getBoardSizeY() - 2) + 1;
 
-   for (i = 0; i < 10; i++) {
-       for (j = 0; j < 30; j++) {
-           candidateCoords[i][j] = 0;
-       }
-   }
+        overlap = false;
+        for (i = 0; i < playerPosList.getSize(); i++) {
+            objPos tempPos;
+            playerPosList.getElement(tempPos, i);
 
+            if (tempPos.isPosEqual(&foodPos)) {
+                overlap = true;
+                break;  // Exit the loop if overlap is found
+            }
+        }
+    } while (overlap);
 
-   do {
-       foodPos.x = rand() % mainGameMechsRef->getBoardSizeX();
-       foodPos.y = rand() % mainGameMechsRef->getBoardSizeY();
-
-
-       // Ensure food position is within bounds
-       foodPos.x = max(1, min(foodPos.x, mainGameMechsRef->getBoardSizeX() - 2));
-       foodPos.y = max(1, min(foodPos.y, mainGameMechsRef->getBoardSizeY() - 2));
-
-
-for (i = 0; i < playerPosList.getSize(); i++)
-{
-
-    if (i >= playerPosList.getSize()) {
-        // Additional safety check to avoid going out of bounds
-        break;
-    }
-    objPos tempPos;
-    playerPosList.getElement(tempPos, i);
-
-
-   if (tempPos.isPosEqual(&foodPos))
-   {
-      flag = 1;
-      
-   }
-}
-
-
-//go through all position
-   } while (flag == 1 ||
-            foodPos.x == 0 || foodPos.y == 0 ||
-            foodPos.x == mainGameMechsRef->getBoardSizeX() - 1 ||
-            foodPos.y == mainGameMechsRef->getBoardSizeY() - 1 ||
-            candidateCoords[foodPos.x][foodPos.y] != 0);
-
-
-   // Mark the generated coordinates as used
-   candidateCoords[foodPos.x][foodPos.y] = 1;
-
-
-   // Print a message to indicate that the food has been generated
-   cout << "Food generated successfully at position: (" << foodPos.x << ", " << foodPos.y << ")" << endl;
+    // Print a message to indicate that the food has been generated
+    //cout << "Food generated successfully at position: (" << foodPos.x << ", " << foodPos.y << ")" << endl;
 }
 void Food::getFoodPos(objPos &returnPos)
 {
    // Step 2: Food objPos getter method
    returnPos = foodPos;
-
-
 }
