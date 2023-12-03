916b06d4737990bc492edba03a45d329cf5e49df
Jenisha Thevarajah <144248040+thevaj5@users.noreply.github.com>
2023-12-03 17:06:26 -0500

    Add files via upload

--- Food.cpp
+++ Food.cpp
@@ -14,10 +14,12 @@ Food::Food(GameMechs* thisGMRef)
     //assign pointer to input
     mainGameMechsRef = thisGMRef;
     foodPos.setObjPos(2, 2, 'o'); //mainGameMechsRef->getBoardSizeX()/2, mainGameMechsRef->getBoardSizeY()/2, 'o');
+    foodBucket = new objPosArrayList();
 }
 
 Food::~Food()
 {
+    //delete foodBucket;
     cout << "Destructor Called" << endl;
 }
 
@@ -49,9 +51,52 @@ void Food::generateFood(objPosArrayList& playerPosList)
     // Print a message to indicate that the food has been generated
     //cout << "Food generated successfully at position: (" << foodPos.x << ", " << foodPos.y << ")" << endl;
 }
+
+
+void Food::generateSpecialFood(objPosArrayList& playerPosList) {
+    for (int i = 0; i < 3; ++i) {
+        generateFood(playerPosList);
+
+        objPos regularFoodPos;
+        getFoodPos(regularFoodPos);
+        foodBucket->insertTail(regularFoodPos);
+        
+    }
+
+    // Generate one special food item
+    bool overlap = true;
+    do {
+        // Generate random coordinates within bounds
+        foodPos.x = rand() % (mainGameMechsRef->getBoardSizeX() - 2) + 1;
+        foodPos.y = rand() % (mainGameMechsRef->getBoardSizeY() - 2) + 1;
+
+        overlap = false;
+        for (int i = 0; i < playerPosList.getSize(); i++) {
+            objPos tempPos;
+            playerPosList.getElement(tempPos, i);
+
+            if (tempPos.isPosEqual(&foodPos)) {
+                overlap = true;
+                break;  
+            }
+        }
+    } while (overlap);
+
+    foodPos.symbol = 'X';  
+
+    foodBucket->insertTail(foodPos);
+}
+
 void Food::getFoodPos(objPos &returnPos)
 {
     // Step 2: Food objPos getter method
+    returnPos = foodPos;
+
+}
+
+void Food::getSpecialFoodPos(objPos& returnPos) {
+
+    //returnPos = specialFoodPos;
     returnPos = foodPos;
 
 }
