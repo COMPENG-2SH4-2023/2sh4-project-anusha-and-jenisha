57929f1c3e1a7bdd3ab7780ead3f65e1743f225e
Jenisha Thevarajah <144248040+thevaj5@users.noreply.github.com>
2023-12-03 23:12:03 -0500

    Add files via upload

--- Food.cpp
+++ Food.cpp
@@ -13,28 +13,13 @@ Food::Food(GameMechs* thisGMRef)
     //game mech reference input - pointer
     //assign pointer to input
     mainGameMechsRef = thisGMRef;
-    //foodPos.setObjPos(2, 2, 'o'); //mainGameMechsRef->getBoardSizeX()/2, mainGameMechsRef->getBoardSizeY()/2, 'o');
-
-    //bonus
-    objPos tempFoodPos;
-    tempFoodPos.setObjPos(2, 2, 'o');
-    foodBucket = new objPosArrayList();
-    foodBucket->insertHead(tempFoodPos); //generateSpecialFood();
-
-
-    //objPos tempPos;
-    //tempPos.setObjPos(mainGameMechsRef->getBoardSizeX()/2, mainGameMechsRef->getBoardSizeY()/2, '*');
-
-    //create an array on the heap
-    //playerPosList = new objPosArrayList();
-    //playerPosList->insertHead(tempPos);
-    //playerPosList->insertHead(tempPos);
+    foodPos.setObjPos(2, 2, 'o');
 
 }
 
 Food::~Food()
 {
-    cout << "Destructor Called" << endl;
+
 }
 
 void Food::generateFood(objPosArrayList& playerPosList)
@@ -61,128 +46,13 @@ void Food::generateFood(objPosArrayList& playerPosList)
             }
         }
     } while (overlap);
-
-    // Print a message to indicate that the food has been generated
-    //cout << "Food generated successfully at position: (" << foodPos.x << ", " << foodPos.y << ")" << endl;
 }
-
-void Food::generateSpecialFood(objPosArrayList& playerPosList) 
-{
-    
-    //generate the normal 3 food items
-    for (int i = 0; i < 3; ++i) {
-        generateFood(playerPosList);
-
-        objPos regularFoodPos;
-        getFoodPos(regularFoodPos);
-        regularFoodPos.symbol = 'o'; 
-
-        foodBucket->insertTail(regularFoodPos);
-        
-    }
-
-
-    // Generate one special food item
-    bool overlap = true;
-    do {
-        // Generate random coordinates within bounds
-        foodPos.x = rand() % (mainGameMechsRef->getBoardSizeX() - 2) + 1;
-        foodPos.y = rand() % (mainGameMechsRef->getBoardSizeY() - 2) + 1;
-
-        overlap = false;
-
-
-   /////////check if there's overlap of special food with previously generated food
-           //must loop through foodbucket array of normal food
-            for(int j =0; j < 3 ; j++){ //foodBucket->getSize()
-
-                //getting the position of the normal food generated above
-                objPos tempFoodPart;
-                foodBucket->getElement(tempFoodPart, j);
-
-                if (tempFoodPart.isPosEqual(&foodPos)) {
-                overlap = true;
-                break;  
-                }
-            }
-            //this breaks out of the outer for loop so we can generate new coords
-            if(overlap == false){
-             
-        for (int i = 0; i < playerPosList.getSize(); i++) {
-
-           //getting the snake body position
-            objPos tempPos;
-            playerPosList.getElement(tempPos, i);
-
-/*
-            /////////check if there's overlap of special food with previously generated food
-           //must loop through foodbucket array of normal food
-            for(int j =0; j < foodBucket->getSize(); j++){
-
-                //getting the position of the normal food generated above
-                objPos tempFoodPart;
-                foodBucket->getElement(tempFoodPart, j);
-
-                if (tempFoodPart.isPosEqual(&foodPos)) {
-                overlap = true;
-                break;  
-            }
-            //this breaks out of the outer for loop so we can generate new coords
-            if(overlap == true){
-                break;
-            }
-          */
-                //}
-
-            //check if there's overlap with the snake body
-            if (tempPos.isPosEqual(&foodPos)) {
-                overlap = true;
-                break;  
-            }
-        }
-            }
-
-    } while (overlap);
-
-    foodPos.symbol = 'X';  
-
-    foodBucket->insertTail(foodPos);
-}
-
-
-
-
 
 
 void Food::getFoodPos(objPos &returnPos)
 {
-    // Step 2: Food objPos getter method
-    returnPos = foodPos;
+   // Step 2: Food objPos getter method
+   returnPos = foodPos;
 
-}
-
-
-
-//should loop through the 3 normal food items in foodBucket and check collision
-void Food::getSpecialFoodPos(objPos& returnPos) {
-
-    //returnPos = specialFoodPos;
-    returnPos = foodPos;
-    
-}
-
-void Food::getFoodBucketElement(objPos& returnPos, int index)
-{
-    foodBucket->getElement(returnPos, index);
-}
-
-
-//get the tail element of the foodBucket
-void Food::getSpecialFoodPosTail(objPos& returnPos) {
-
-    
-    //playerPosList.getTailElement(additions);
-    foodBucket->getTailElement(returnPos);
-    //returnPos = foodPos;
 
 }
